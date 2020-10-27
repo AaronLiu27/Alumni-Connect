0168d88b24deae7ca65afe417cf2f3cf1e3acca0
Vivian004 <vivianbieb@gmail.com>
2020-10-27 02:05:24 -0400

    Improved test coverage

--- source/backend/db.py
+++ source/backend/db.py
@@ -1,33 +1,13 @@
 from flask_pymongo import PyMongo
-
-# from flask_mongoengine import MongoEngine
-
 from dotenv import load_dotenv
 import os
 
-import json
-from bson.objectid import ObjectId
-import datetime
-
-# Load private environment configuration
-# You need your own .env file
 load_dotenv()
 MONGO_URI = os.getenv("MONGO_URI")
 if not MONGO_URI:
     MONGO_URI = "mongodb+srv://" + \
         "vivian:123456a@alumniconnect.usbk3.mongodb.net" + \
         "/db?retryWrites=true&w=majority"
-
-
-class JSONEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, ObjectId):
-            return str(obj)
-        if isinstance(obj, set):
-            return list(obj)
-        if isinstance(obj, datetime.datetime):
-            return str(obj)
-        return json.JSONEncoder.default(self, obj)
 
 
 mongo = PyMongo(connect=False)
