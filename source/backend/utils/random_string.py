2deb7dfdb4b3d5ac03fa683a7a1e2873fd2c0ace
Vivian004 <vivianbieb@gmail.com>
2020-11-03 13:33:17 -0500

    Reorganized tests

--- source/backend/utils/random_string.py
+++ source/backend/utils/random_string.py
@@ -1,5 +1,6 @@
 def get_random_string(length=8, type="ascii_letters"):
-    import random, string
+    import random
+    import string
     letters = getattr(string, type, "")
     result_str = "".join(random.choice(letters) for i in range(length))
     return result_str
