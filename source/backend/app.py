0a17e7fa114b379f3a58866626a110d3c8841d5d
Vivian004 <vivianbieb@gmail.com>
2020-11-03 04:16:21 -0500

    Modified app config

--- source/backend/app.py
+++ source/backend/app.py
@@ -36,7 +36,9 @@ jwt = JWTManager(app)
 
 
 if __name__ == "__main__":
+    host = os.environ.get('HOST', '0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
     app.run(
-        host=app.config["HOST"] or "0.0.0.0",
-        port=app.config["PORT"] or int(os.environ.get('PORT', 5000))
+        host=host,
+        port=port
     )
