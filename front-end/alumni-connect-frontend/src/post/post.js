4edcb63dbe909ac3b9b7f034adde2ab9ccdab144
ed wang <ed@runtheworld.today>
2020-11-09 10:03:02 -0500

    add post demo

--- front-end/alumni-connect-frontend/src/post/post.js
+++ front-end/alumni-connect-frontend/src/post/post.js
@@ -4,17 +4,46 @@ import UserStore from '../stores/UserStore';
 import {useHistory} from "react-router-dom";
 import axios from 'axios';
 import {Route, Switch, Link, BrowserRouter as Router} from "react-router-dom";
-import {Button} from 'react-bootstrap';
+import {Button, Card, ListGroup, ListGroupItem} from 'react-bootstrap';
 import { useState } from "react";
 import "./post.css";
 
 function PostList() {
+const post = [
+    {id:1, title:'www'},
+    {id:2, title:'www1'},
+    {id:3, title:'www2'},
+    {id:4, title:'www3'},
+    {id:5, title:'www4'}
+
+]
+    
 
 
 
     return (
         <div>
-            Hi Post
+            {
+                post.map(p=>
+                    <div >
+                        <Card >
+                            <Card.Body>
+                                <Card.Title>{p.title}</Card.Title>
+                                <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                                </Card.Text>
+                            </Card.Body>
+                            <ListGroup className="list-group-flush">
+                                <ListGroupItem>Cras justo odio</ListGroupItem>
+                                <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+                                <ListGroupItem>Vestibulum at eros</ListGroupItem>
+                            </ListGroup>
+                        </Card>
+                    </div>
+                )
+            }
+            
         </div>
     );
 
