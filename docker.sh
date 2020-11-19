90c41c0dda3a18a230f0fc03301dd0ffd451bda2
Jingxian Du <jd4472@nyu.edu>
2020-11-19 13:50:31 -0800

    Removed volumn from docker

--- docker.sh
+++ docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 docker build -t alumni-connect .
 docker rm alumni-connect-run
-docker run -p 5000:5000 -v $PWD:/app:rw --name alumni-connect-run alumni-connect gunicorn wsgi:app -b 0.0.0.0:5000
+docker run -p 5000:5000 --name alumni-connect-run alumni-connect gunicorn wsgi:app -b 0.0.0.0:5000
