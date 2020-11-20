54c796934d3635e66dbb21b16cf2e88226181043
CC2019 <458123902@qq.com>
2020-11-19 22:32:47 -0800

    set port for frontend docker container

--- front-end/alumni-connect-frontend/docker.sh
+++ front-end/alumni-connect-frontend/docker.sh
@@ -2,5 +2,8 @@
 
 docker build -t frontenddocker . 
 
+export PORT=3000
+export LOCAL_PORT=8000
+
 docker rm -f reactindocker
-docker run -p 8000:3000 --name reactindocker frontenddocker
+docker run -p $LOCAL_PORT:$PORT --name reactindocker frontenddocker
