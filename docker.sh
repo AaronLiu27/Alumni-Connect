96a8bbc3caf60a0c3a6dc822f54e267ac26416b0
Jingxian Du <jd4472@nyu.edu>
2020-12-05 01:01:37 -0500

    Fixed docker.sh

--- docker.sh
+++ docker.sh
@@ -5,5 +5,5 @@ docker build -t nyudevops/alumniconnect:latest .
 export PORT=5000
 export LOCAL_PORT=5000
 
-# docker rm -f alumni-connect-run
+docker rm -f alumni-connect-run 2> /dev/null || true
 docker run --env PORT=$PORT -p $LOCAL_PORT:$PORT --name alumni-connect-run nyudevops/alumniconnect:latest
