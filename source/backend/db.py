94470326f9aa829c91753d8fc2371fd571419596
Jingxian Du <jd4472@nyu.edu>
2020-12-08 12:14:35 -0500

    Temporarily added env var for TA usage

--- source/backend/db.py
+++ source/backend/db.py
@@ -5,7 +5,7 @@ import os
 load_dotenv()
 MONGO_URI = os.getenv("MONGO_URI")
 if not MONGO_URI:
-    MONGO_URI = os.environ.get("MONGO_URI")
+    MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://vivian:123456a@alumniconnect.usbk3.mongodb.net/db?retryWrites=true&w=majority")
 
 
 mongo = PyMongo(connect=False)
